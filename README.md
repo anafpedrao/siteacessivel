e8f7543ddfba4a1a3f0ad49836246d3048cbabb2
Ana Flavia Pedrão <a.pedrao@escola.pr.gov.br>
2025-03-09 17:58:47 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# siteacessivel
-Site acessivel - tema: solos
+# Site acessível sobre Solos
+# Sobre
+Construção e refatoração de um site, implementando recursos de acessibilidade no html, css e JS.
+## Recursos de acessibilidade
+- Atributos aria
+- alt
+- tab-index
+- Menu de acessibilidade
+
+## Tecnologias utilizadas
+- Bootstrap
+- ScrollRevealjs
+- HTML
+- CSS
+- JS
+
--- script.js
+++ script.js
@@ -2,13 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-    botaoDeAcessibilidade.addEventListener('click', function () {
+    botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
     })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
 
     let tamanhoBase = 18; // Tamanho inicial do corpo do texto
     let fatorTitulos = 1.5; // Os títulos serão sempre 1.5x maiores que o texto normal
@@ -48,4 +52,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Aplicar os tamanhos iniciais ao carregar a página
     alterarTamanhoFonte(tamanhoBase);
 
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
 });
+
+ScrollReveal().reveal('#home', { delay: 500 });
+ScrollReveal().reveal('#about', { delay: 500 });
+ScrollReveal().reveal('#contact', { delay: 500 });
